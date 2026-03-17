f90b9cbd080d10f95d43d6d96b55ec525dea881e
Lucifer <48712492+dkconnect@users.noreply.github.com>
2026-03-17 20:22:05 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,16 +1,3 @@
-function formatSize(bytes){
-return (bytes/1024/1024).toFixed(2)+" MB"
-}
-
-function downloadBlob(blob,filename){
-
-const url=URL.createObjectURL(blob)
-
-const a=document.createElement("a")
-a.href=url
-a.download=filename
-a.click()
-
-URL.revokeObjectURL(url)
-
+function goHome(){
+window.location.href = "../../index.html"
 }

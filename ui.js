cfbb7fdb06192af4a915b79373769353f4920187
Lucifer <48712492+dkconnect@users.noreply.github.com>
2026-03-17 23:33:44 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,3 +1,39 @@
 function goHome(){
-window.location.href = "../../index.html"
+window.location.href="../../index.html"
+}
+
+/* reusable upload system */
+
+function setupUpload(zoneId,inputId,onFiles){
+
+const zone=document.getElementById(zoneId)
+const input=document.getElementById(inputId)
+
+zone.onclick=()=>input.click()
+
+input.addEventListener("change",e=>{
+if(e.target.files.length){
+onFiles(e.target.files)
+}
+})
+
+zone.addEventListener("dragover",e=>{
+e.preventDefault()
+zone.classList.add("drag")
+})
+
+zone.addEventListener("dragleave",()=>{
+zone.classList.remove("drag")
+})
+
+zone.addEventListener("drop",e=>{
+e.preventDefault()
+zone.classList.remove("drag")
+
+if(e.dataTransfer.files.length){
+onFiles(e.dataTransfer.files)
+}
+
+})
+
 }
